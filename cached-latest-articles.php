592a07d9c08e6a6b2a4ce422a9bc7189f63b968b
Vasilis Dellas <dellas.vasileios2@gmail.com>
2025-04-06 20:55:39 +0300

    added basic folder and file structure

---
+++ cached-latest-articles.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: Cached Latest Articles
+ * Description: Displays the latest articles using a JSON cache. Supports card or list view through a shortcode.
+ * Version: 1.0
+ * Author: Vasilis Dellas
+ */
+
+ if(!defined('ABSPATH')) {
+    exit; // Exit if accessed directly
+}
